97e18d51b357f3db3dbb11bd240803d472b3c7c3
raylu <90059+raylu@users.noreply.github.com>
2023-05-12 00:34:50 -0700

    take an arg to start at another rev

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,33 @@
 use git2::Repository;
-use std::{env, path::Path};
+use std::{
+	env,
+	path::{Path, PathBuf},
+};
 
 mod git;
 mod terminal;
 
 fn main() {
 	let args: Vec<String> = env::args().collect();
-	if args.len() != 2 {
-		println!("usage: {} <filepath>", args[0].rsplit('/').next().unwrap());
+	if args.len() < 2 || args.len() > 3 {
+		println!("usage: {} <filepath> [rev]", args[0].rsplit('/').next().unwrap());
 		return;
 	}
-	let abs_path_buf = Path::new(&args[1]).canonicalize().unwrap();
-	let abs_path = abs_path_buf.as_path();
 
-	let repo = match Repository::discover(abs_path) {
-		Ok(repo) => repo,
-		Err(e) => panic!("{}", e),
+	let path = Path::new(&args[1]);
+	let (repo, abs_path) = match path.canonicalize() {
+		Ok(abs_path) => (Repository::discover(&abs_path).unwrap(), abs_path),
+		Err(_) => (Repository::open_from_env().unwrap(), path_to_lexical_absolute(path)),
 	};
-	let rel_path = abs_path.strip_prefix(repo.workdir().unwrap()).unwrap();
+	let rel_path = abs_path.strip_prefix(repo.workdir().unwrap()).unwrap().to_owned();
 
-	let head = repo.head().unwrap().target().unwrap();
-	let mut app = terminal::App::new(&repo, rel_path, head);
-	app.blame = match git::blame(&repo, rel_path, head) {
+	let commit = if args.len() == 3 {
+		repo.revparse_single(&args[2]).unwrap().id()
+	} else {
+		repo.head().unwrap().target().unwrap()
+	};
+	let mut app = terminal::App::new(&repo, &rel_path, commit);
+	app.blame = match git::blame(&repo, &rel_path, commit) {
 		Ok(blame) => blame,
 		Err(e) => panic!("{}", e),
 	};
@@ -32,4 +38,23 @@ fn main() {
 	if let Err(err) = res {
 		println!("{:?}", err)
 	}
+}
+
+fn path_to_lexical_absolute(path: &Path) -> PathBuf {
+	// https://internals.rust-lang.org/t/path-to-lexical-absolute/14940
+	let mut absolute = if path.is_absolute() {
+		PathBuf::new()
+	} else {
+		std::env::current_dir().unwrap()
+	};
+	for component in path.components() {
+		match component {
+			std::path::Component::CurDir => {}
+			std::path::Component::ParentDir => {
+				absolute.pop();
+			}
+			component => absolute.push(component.as_os_str()),
+		}
+	}
+	absolute
 }
